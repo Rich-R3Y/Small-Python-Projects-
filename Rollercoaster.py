4d55fc296cb782196332cc834a48a5556d5a3574
Rich-R3Y <150986393+Rich-R3Y@users.noreply.github.com>
2025-03-26 19:02:05 -0700

    Update Rollercoaster.py

--- Rollercoaster.py
+++ Rollercoaster.py
@@ -2,5 +2,12 @@ print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 if height >= 120:
     print("You can ride the rollercoaster")
+    age = int(input("What is your age? "))
+    if age <= 12:
+        print("You have to pay 5$")
+    elif age <= 18:
+        print("You have to pay 7$")
+    else:
+        print("You have to pay 12$")
 else:
-    print("You are too short to ride the rollercoaster")
+    print("Sorry you have to grow taller before you can ride.")
