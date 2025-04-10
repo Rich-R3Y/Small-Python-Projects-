db32044962e11648fe9a4c7fa63afbaa42e0da5c
Rich-R3Y <150986393+Rich-R3Y@users.noreply.github.com>
2025-04-10 02:14:15 -0700

    Create caesar_cipher.py

---
+++ caesar_cipher.py
@@ -0,0 +1,32 @@
+from art import logo
+print()
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+    for i in original_text:
+        if i not in alphabet:
+            output_text += i
+        else:
+            shifted_position = alphabet.index(i) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+second_run = False
+while not second_run:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    go_on =  input('Type "Yes" if you want to run the program again. Otherwise, type "no"\n').lower()
+    if go_on == 'no':
+        second_run = True
+
